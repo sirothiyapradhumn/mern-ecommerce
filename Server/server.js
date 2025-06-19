049d3371c1b095fdb98c9b4ac45c8d79c94f4aca
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-19 23:53:30 +0530

    feat(server): initialize server with Express and MongoDB connection

---
+++ Server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiesParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/", // Adjust this to your frontend URL
+    credentials: true, // Allow cookies to be sent
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+  })
+);
+
+app.use(cookiesParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
