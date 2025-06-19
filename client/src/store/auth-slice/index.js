6b7e2a35cc4e2d8c0ad806b8ef4230b5b32a625f
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-19 23:52:59 +0530

    feat: initialize client application with React, Redux, and Tailwind CSS

---
+++ client/src/store/auth-slice/index.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: () => {},
+  },
+});
+
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
