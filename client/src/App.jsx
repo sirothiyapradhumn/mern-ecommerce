a19feaa0f4568fa34004002be6bfacf41893f74b
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-20 23:55:17 +0530

    feat(client): add authentication check and unauthorized access page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,77 @@
-import { Button } from "@/components/ui/button"
-import { Route, Routes } from "react-router-dom"
-import AuthLayout from "./components/auth/AuthLayout"
-import AuthLogin from "./pages/auth/AuthLogin"
-import AuthRegister from "./pages/auth/AuthRegister"
-import AdminLayout from "./components/admin-view/AdminLayout"
-import Dashboard from "./pages/admin-view/Dashboard"
-import Products from "./pages/admin-view/Products"
-import Orders from "./pages/admin-view/Orders"
-import Features from "./pages/admin-view/Features"
-import ShoppingLayout from "./components/shopping-view/ShoppingLayout"
-import NotFound from "./pages/notFound/NotFound"
-import ShoppingAccount from "./pages/shopping-view/ShoppingAccount"
-import ShoppingCheckout from "./pages/shopping-view/ShoppingCheckout"
-import ShoppingHome from "./pages/shopping-view/ShoppingHome"
-import ShoppingListing from "./pages/shopping-view/ShoppingListing"
+import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/AuthLayout";
+import AuthLogin from "./pages/auth/AuthLogin";
+import AuthRegister from "./pages/auth/AuthRegister";
+import AdminLayout from "./components/admin-view/AdminLayout";
+import Dashboard from "./pages/admin-view/Dashboard";
+import Products from "./pages/admin-view/Products";
+import Orders from "./pages/admin-view/Orders";
+import Features from "./pages/admin-view/Features";
+import ShoppingLayout from "./components/shopping-view/ShoppingLayout";
+import NotFound from "./pages/notFound/NotFound";
+import ShoppingAccount from "./pages/shopping-view/ShoppingAccount";
+import ShoppingCheckout from "./pages/shopping-view/ShoppingCheckout";
+import ShoppingHome from "./pages/shopping-view/ShoppingHome";
+import ShoppingListing from "./pages/shopping-view/ShoppingListing";
+import CheckAuth from "./components/common/CheckAuth";
+import UnAuth from "./pages/unauth/UnAuth";
 
 function App() {
+  const isAuthenticated = true;
+  const user = {
+    name: "Sahil",
+    role: "admin",
+  };
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="products" element={<Products />} />
           <Route path="orders" element={<Orders />} />
           <Route path="features" element={<Features />} />
         </Route>
 
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="home" element={<ShoppingHome />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="listing" element={<ShoppingListing />} />
-        </Route>  
+        </Route>
 
+        <Route path="/unauth-page" element={<UnAuth />} />
         <Route path="*" element={<NotFound />} />
-
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/unauth/UnAuth.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UnAuth() {
+  return <div>You don't have access to view this page.</div>;
+}
+
+export default UnAuth;
